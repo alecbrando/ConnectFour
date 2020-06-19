585d5fccb0b51cb60d7de6f2517bf08aca771956
Alec Garcia <alecgarcia@Alecs-iMac.local>
2020-06-19 14:49:08 -0700

    Working board without logic

--- connect-four.js
+++ connect-four.js
@@ -1,59 +1,104 @@
-import Game from './game.js'; 
+import Game from './game.js';
+import Column from './column.js'; 
 let game;
 const board = document.getElementById('board-holder');
-if(game == undefined){
-    board.classList.add('is-invisible');
-}
-board.classList.add('is-invisible');
+
+function updateUI(){
+    
+
+    if (game === undefined) {
+        board.classList.add('is-invisible');
+    }
+
+    const gameName = document.getElementById('game-name');
+    gameName.innerHTML = game.getName();
+    const clickTarget = document.getElementById('click-targets');
+
+        let currentPlayer = game.getCurrentPlayer();
+        if (currentPlayer === 1) {
+            clickTarget.classList.remove('red');
+            clickTarget.classList.add('black');
+        } else if (currentPlayer === 2) {
+            clickTarget.classList.add('red');
+            clickTarget.classList.remove('black');
+        }
+        for (let i = 0; i <= 5; i++) {
+            let row = i;
+            for (let j = 0; j <= 6; j++) {
+                let column = j;
+                let square = document.querySelector(`#square-${row}-${column}`);
+                //console.log(square);
+                square.innerHTML = '';
+                let playerNumber = game.getTokenAt(column, row);
+                
+                if (playerNumber === 1) {
+                    let token = document.createElement('div');
+                    // token.classList.add('token');
+                    token.classList.add('token')
+                    token.classList.add('black')
+                    square.appendChild(token);
+                } else if (playerNumber === 2) {
+                    let token = document.createElement('div');
+                    token.classList.add('token')
+                    token.classList.add('red');
+                    square.appendChild(token);
+                }
+            }            
+        }
+
+};
 
 window.addEventListener("DOMContentLoaded", (event) => {
     // const clickTarget = document.getElementById('click-targets');
     // clickTarget.addEventListener('mouseover', event => {
     //     event.target.classList.add('black');
     // });
-    
+
+    board.classList.add('is-invisible');
     const playerOne = document.getElementById('player-one-name');
     const playerTwo = document.getElementById('player-two-name');
     const newButton = document.getElementById('new-game');
     let playerTwoValue; 
     let playerOneValue;
     
-    playerOne.addEventListener('keyup', (event) => {
+    function inputFieldNames(){
         playerOneValue = playerOne.value;
-            if (!playerOneValue) { 
-                // console.log('hi')
-                newButton.disabled = true;
-            }
-        if (playerTwoValue && playerOneValue) {
-            newButton.disabled = false;
-        }
-        
-                
-    });
-    playerTwo.addEventListener('keyup', (event) => {
         playerTwoValue = playerTwo.value;
-        
-        if (!playerTwoValue) { 
-            // console.log('hi')
+
+        if (!playerTwoValue) {
             newButton.disabled = true;
         }
         if (playerTwoValue && playerOneValue) {
             newButton.disabled = false;
         }
-        
-        
+
+    }
+
+    playerOne.addEventListener('keyup', (event) => {
+        inputFieldNames();
+    });
+    playerTwo.addEventListener('keyup', (event) => {
+        inputFieldNames();
     });
 
     newButton.addEventListener('click', (event) => {
         game = new Game(playerOneValue, playerTwoValue);
         board.classList.remove('is-invisible');
-        // console.log(playerOne.value, playerTwo.value)
         playerOne.value = '';
         playerTwo.value = '';
-        // console.log(playerOne.value, playerTwo.value)
+        updateUI();
     });
-    
-
+    const test = new Column;
+    // console.log(test.getTokenAt(3));
+    document.getElementById('click-targets').addEventListener('click', event => {
+        let tempId = event.target.id;
+        let tempArr = tempId.split('-');
+        let columnNum = Number(tempArr[1]);
+        // console.log(columnNum);
+        game.playInColumn(columnNum);
+        console.log(game.columns);
+        updateUI();
+    })
 
    
 });
--- game.js
+++ game.js
@@ -1,6 +1,35 @@
+import Column  from "./column.js"
+
 export default class Game {
     constructor(player1, player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.currentPlayer = 1;
+        this.columns = [new Column(), new Column(), new Column(), new Column(),
+                        new Column(), new Column(), new Column()];
+    }
+
+    getName() {
+        return `${this.player1} vs ${this.player2}`;
+    }
+
+    playInColumn(columnIndex) {
+        this.columns[columnIndex].add(this.currentPlayer);
+        
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
+    }
+
+    getCurrentPlayer() {
+        return this.currentPlayer;
+    }
+
+    getTokenAt(colIndex, rowIndex) {
+        return this.columns[colIndex].getTokenAt(rowIndex);
     }
 }
+
+
